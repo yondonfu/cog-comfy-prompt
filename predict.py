85b60867ce2ab5fc03ed5194bd3701de487e7deb
Yondon Fu <yondon.fu@gmail.com>
2023-12-21 15:48:43 -0500

    WIP

---
+++ predict.py
@@ -0,0 +1,113 @@
+import subprocess
+import threading
+import time
+from cog import BasePredictor, Input, Path, File
+import uuid
+import json
+import urllib
+import websocket
+from PIL import Image
+from urllib.error import URLError
+
+
+class Predictor(BasePredictor):
+    def setup(self):
+        self.server_address = "127.0.0.1:8188"
+        self.start_server()
+
+    def start_server(self):
+        server_thread = threading.Thread(target=self.run_server)
+        server_thread.start()
+
+        while not self.is_server_running():
+            time.sleep(1)
+
+        print("Server is up and running!")
+
+    def run_server(self):
+        command = "python ComfyUI/main.py"
+        server_process = subprocess.Popen(command, shell=True)
+        server_process.wait()
+
+    # hacky solution, will fix later
+    def is_server_running(self):
+        try:
+            with urllib.request.urlopen("http://{}/history/{}".format(self.server_address, "123")) as response:
+                return response.status == 200
+        except URLError:
+            return False
+
+    def queue_prompt(self, prompt, client_id):
+        p = {"prompt": prompt, "client_id": client_id}
+        data = json.dumps(p).encode('utf-8')
+        req = urllib.request.Request(
+            "http://{}/prompt".format(self.server_address), data=data)
+        return json.loads(urllib.request.urlopen(req).read())
+
+    def get_image(self, filename, subfolder, folder_type):
+        data = {"filename": filename,
+                "subfolder": subfolder, "type": folder_type}
+        print(folder_type)
+        url_values = urllib.parse.urlencode(data)
+        with urllib.request.urlopen("http://{}/view?{}".format(self.server_address, url_values)) as response:
+            return response.read()
+
+    def get_images(self, ws, prompt, client_id):
+        prompt_id = self.queue_prompt(prompt, client_id)['prompt_id']
+        output_images = {}
+        while True:
+            out = ws.recv()
+            if isinstance(out, str):
+                message = json.loads(out)
+                if message['type'] == 'executing':
+                    data = message['data']
+                    if data['node'] is None and data['prompt_id'] == prompt_id:
+                        break  # Execution is done
+            else:
+                continue  # previews are binary data
+
+        history = self.get_history(prompt_id)[prompt_id]
+        for o in history['outputs']:
+            for node_id in history['outputs']:
+                node_output = history['outputs'][node_id]
+                print("node output: ", node_output)
+
+                if 'images' in node_output:
+                    images_output = []
+                    for image in node_output['images']:
+                        image_data = self.get_image(
+                            image['filename'], image['subfolder'], image['type'])
+                        images_output.append(image_data)
+                output_images[node_id] = images_output
+
+        return output_images
+
+    def get_history(self, prompt_id):
+        with urllib.request.urlopen("http://{}/history/{}".format(self.server_address, prompt_id)) as response:
+            return json.loads(response.read())
+
+    def predict(
+        self,
+        prompt_file: File = Input(
+            description="File with ComfyUI API prompt in JSON format"),
+    ) -> Path:
+        prompt = json.loads(prompt_file)
+        img_output_path = self.get_prompt_output(
+            prompt=prompt,
+        )
+        return Path(img_output_path)
+
+    def get_prompt_output(self, prompt) -> Path:
+        client_id = str(uuid.uuid4())
+        ws = websocket.WebSocket()
+        ws.connect(
+            "ws://{}/ws?clientId={}".format(self.server_address, client_id))
+        images = self.get_images(ws, prompt, client_id)
+
+        for node_id in images:
+            for image_data in images[node_id]:
+                from PIL import Image
+                import io
+                image = Image.open(io.BytesIO(image_data))
+                image.save("out-"+node_id+".png")
+                return Path("out-"+node_id+".png")
